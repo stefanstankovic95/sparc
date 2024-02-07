6e712ecf2f3434006ca304a53a8a2ee4e1bf3775
Stefan Stankovic <stefantesla995@gmail.com>
2024-02-07 20:56:13 +0100

    - Add home

--- sparc-ui/src/App.tsx
+++ sparc-ui/src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Home } from './containers/Home';
 
 function App() {
   return (
@@ -19,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Home />
     </div>
   );
 }
