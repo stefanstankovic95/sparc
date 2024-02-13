ab5fb599eb1b111b608420038c2646fbdd5796d2
Stefan Stankovic <stefantesla995@gmail.com>
2024-02-13 23:07:20 +0100

    - Add logo

--- sparc-ui/src/containers/Layout.tsx
+++ sparc-ui/src/containers/Layout.tsx
@@ -1,7 +1,10 @@
 import Link from "@mui/material/Link";
 import { Box } from "@mui/system";
+import logo from "../logo.png";
 
 export const Layout = (props: any) => {
+  const showLogo = !["/", "/teams"].includes(window.location.pathname);
+
   return (
     <Box>
       <Box display="flex" justifyContent="space-evenly" paddingTop={5}>
@@ -61,6 +64,17 @@ export const Layout = (props: any) => {
           </Link>
         </Box>
       </Box>
+      {showLogo && (
+        <Box position="absolute">
+          <img
+            style={{ paddingLeft: 60 }}
+            src={logo}
+            loading="lazy"
+            height={200}
+            width={200}
+          />
+        </Box>
+      )}
       {props.children}
     </Box>
   );
