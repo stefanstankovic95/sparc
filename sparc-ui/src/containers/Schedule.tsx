0410775028402722f2e2e72be575b0a625577fd1
Stefan Stankovic <stefantesla995@gmail.com>
2024-02-13 20:51:48 +0100

    - Latest changes

--- sparc-ui/src/containers/Schedule.tsx
+++ sparc-ui/src/containers/Schedule.tsx
@@ -1,3 +1,158 @@
+import { Box, Chip, Grid, Typography } from "@mui/material";
+import data from "../files/schedule.json";
+import vsIcon from "../files/vs-icon.png";
+import {
+  Timeline,
+  TimelineContent,
+  TimelineDot,
+  TimelineItem,
+  TimelineOppositeContent,
+  TimelineSeparator,
+} from "@mui/lab";
+
 export const Schedule = () => {
-  return <></>;
+  return (
+    <Box>
+      <Grid container>
+        <Grid item xs={12} paddingTop={10}>
+          <Typography fontSize={36} color="khaki" fontFamily="impact">
+            Stream
+          </Typography>
+          <Timeline position="right">
+            {data.streamed.map((match) => (
+              <TimelineItem>
+                <TimelineOppositeContent>
+                  <Typography
+                    className="fire"
+                    fontSize={44}
+                    color="burlywood"
+                    fontFamily="impact"
+                    style={{ animationDelay: "3s", opacity: "90%" }}
+                  >
+                    {match.left}
+                  </Typography>
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineDot
+                    style={{
+                      backgroundColor: "transparent",
+                      marginTop: -5,
+                      boxShadow: "none",
+                    }}
+                  >
+                    <Typography
+                      fontSize={48}
+                      color="orange"
+                      fontFamily="impact"
+                    >
+                      {match.time}
+                    </Typography>
+                  </TimelineDot>
+                </TimelineSeparator>
+                <TimelineContent>
+                  <Typography
+                    className="fire"
+                    fontSize={44}
+                    color="coral"
+                    fontFamily="impact"
+                    style={{ animationDelay: "2.5s", opacity: "75%" }}
+                  >
+                    {match.left}
+                  </Typography>
+                </TimelineContent>
+              </TimelineItem>
+            ))}
+          </Timeline>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography fontSize={48} color="khaki" fontFamily="impact">
+            {data.firstRound.time}
+          </Typography>
+          <Timeline position="right">
+            {data.firstRound.matches.map((match) => (
+              <TimelineItem>
+                <TimelineOppositeContent>
+                  <Chip
+                    style={{
+                      fontSize: 24,
+                      fontFamily: "impact",
+                      color: "burlywood",
+                      borderColor: "burlywood",
+                    }}
+                    label={match.left}
+                    size="medium"
+                    variant="outlined"
+                  />
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineDot
+                    style={{ backgroundColor: "transparent", marginTop: -15 }}
+                  >
+                    <img src={vsIcon} loading="lazy" height={60} width={60} />
+                  </TimelineDot>
+                </TimelineSeparator>
+                <TimelineContent>
+                  <Chip
+                    style={{
+                      fontSize: 24,
+                      color: "coral",
+                      borderColor: "coral",
+                      fontFamily: "impact",
+                    }}
+                    label={match.right}
+                    size="medium"
+                    variant="outlined"
+                  />
+                </TimelineContent>
+              </TimelineItem>
+            ))}
+          </Timeline>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography fontSize={48} color="khaki" fontFamily="impact">
+            {data.secondRound.time}
+          </Typography>
+          <Timeline position="right">
+            {data.secondRound.matches.map((match) => (
+              <TimelineItem>
+                <TimelineOppositeContent>
+                  <Chip
+                    style={{
+                      fontSize: 24,
+                      fontFamily: "impact",
+                      color: "burlywood",
+                      borderColor: "burlywood",
+                    }}
+                    label={match.left}
+                    size="medium"
+                    variant="outlined"
+                  />
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineDot
+                    style={{ backgroundColor: "transparent", marginTop: -15 }}
+                  >
+                    <img src={vsIcon} loading="lazy" height={60} width={60} />
+                  </TimelineDot>
+                </TimelineSeparator>
+                <TimelineContent>
+                  <Chip
+                    style={{
+                      fontSize: 24,
+                      color: "coral",
+                      borderColor: "coral",
+                      fontFamily: "impact",
+                    }}
+                    label={match.right}
+                    size="medium"
+                    variant="outlined"
+                  />
+                </TimelineContent>
+              </TimelineItem>
+            ))}
+          </Timeline>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
--- sparc-ui/src/containers/Teams.tsx
+++ sparc-ui/src/containers/Teams.tsx
@@ -1,3 +1,28 @@
+import { Box, Grid } from "@mui/material";
+
+const cache = {};
+
+function importAll(r: any) {
+  r.keys().forEach((key: any) => ((cache as any)[key] = r(key)));
+}
+// Note from the docs -> Warning: The arguments passed to require.context must be literals!
+importAll(require.context("../team-icons", false, /\.(png|jpe?g|svg)$/));
+
+const images = Object.entries(cache).map(
+  (module) => module[1]
+) as Array<string>;
+
 export const Teams = () => {
-  return <></>;
+  console.log("images", images, Object.entries(cache));
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2} marginTop={10}>
+        {images.map((image) => (
+          <Grid item xs={3}>
+            <img src={image} loading="lazy" height={170} width={150} />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
