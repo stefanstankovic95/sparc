4030055f42aa838653202ea83645bc6cc059b401
BLAZE Esports <sparc@blaze-esports.org>
2024-02-15 17:05:36 +0100

    Added test statistics

--- sparc-ui/src/containers/LeagueStatistic.tsx
+++ sparc-ui/src/containers/LeagueStatistic.tsx
@@ -3,7 +3,7 @@ import { Box } from "@mui/system";
 export const LeagueStatistic = () => {
   return (
       <Box justifyContent="center">
-        <iframe title="SparcReport" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZjQyYjRkNDMtZDU3Ny00NWE4LWI2YjUtMGRmMWE0NTEzNDU0IiwidCI6IjMxODY4OTk5LWE5NTUtNGNiNi04N2E3LTJmZTYwMDk3Y2IwNyIsImMiOjl9" frameBorder={0} allowFullScreen={true}></iframe>
+        <iframe title="SparcReport" width="600px" height="373.5px" src="https://app.powerbi.com/view?r=eyJrIjoiZjQyYjRkNDMtZDU3Ny00NWE4LWI2YjUtMGRmMWE0NTEzNDU0IiwidCI6IjMxODY4OTk5LWE5NTUtNGNiNi04N2E3LTJmZTYwMDk3Y2IwNyIsImMiOjl9" frameBorder={0} allowFullScreen={true}></iframe>
       </Box>
     );
 };
