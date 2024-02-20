f4065d09d794c04a84a2741e5acc27a78fe1ea8b
Stefan Stankovic <stefantesla995@gmail.com>
2024-02-21 00:51:49 +0100

    - Footer

--- sparc-ui/src/App.tsx
+++ sparc-ui/src/App.tsx
@@ -7,11 +7,41 @@ import { Schedule } from "./containers/Schedule";
 import { About } from "./containers/About";
 import { Home } from "./containers/Home";
 import { useEffect } from "react";
+import tiktok from "./link-icons/tiktok.png";
+import instagram from "./link-icons/instagram.png";
+import email from "./link-icons/email.png";
+import youtube from "./link-icons/youtube.png";
+import { Box, Divider, Stack } from "@mui/material";
 
 function App() {
   useEffect(() => {
     document.title = "SPARC";
   }, []);
+
+  const handleYoutubeClick = () => {
+    window.open(
+      "https://www.youtube.com/channel/UCs6ZeNLjcqZI28aATvSYhzw",
+      "_blank"
+    );
+  };
+
+  const handleInstagramClick = () => {
+    window.open(
+      "https://www.instagram.com/sparc_league?igsh=eGxsbXBuajNoNWZo",
+      "_blank"
+    );
+  };
+
+  const handleTiktokClick = () => {
+    window.open(
+      "https://www.tiktok.com/@sparc_league?fbclid=IwAR0Kj_eFvsev-UK6lc9JHsj675kosdN93hU7ptHZjwfg0pJEekxljgV3Lw4",
+      "_blank"
+    );
+  };
+
+  const handleEmailClick = () => {
+    window.open("mailto:sparc@blaze-esports.org", "_blank");
+  };
 
   return (
     <div
@@ -22,6 +52,9 @@ function App() {
         flex: 1,
         height: "100%",
         minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
       }}
     >
       <Layout>
@@ -35,6 +68,55 @@ function App() {
           </Routes>
         </BrowserRouter>
       </Layout>
+
+      <Box width="100%" bgcolor="black">
+        <Divider
+          style={{
+            backgroundColor: "grey",
+          }}
+          flexItem
+        />
+        <Box display="flex" marginTop={5} marginBottom={5}>
+          <Stack
+            width="100%"
+            direction="row"
+            spacing={2}
+            justifyContent="center"
+          >
+            <img
+              style={{ marginTop: 2 }}
+              src={instagram}
+              loading="lazy"
+              height={45}
+              width={45}
+              onClick={handleInstagramClick}
+            />
+            <img
+              src={tiktok}
+              loading="lazy"
+              height={50}
+              width={50}
+              onClick={handleTiktokClick}
+            />
+            <img
+              style={{ marginTop: -4 }}
+              src={youtube}
+              loading="lazy"
+              height={55}
+              width={45}
+              onClick={handleYoutubeClick}
+            />
+            <img
+              style={{ marginTop: 5 }}
+              src={email}
+              loading="lazy"
+              height={40}
+              width={50}
+              onClick={handleEmailClick}
+            />
+          </Stack>
+        </Box>
+      </Box>
     </div>
   );
 }
--- sparc-ui/src/containers/Home.tsx
+++ sparc-ui/src/containers/Home.tsx
@@ -41,47 +41,6 @@ export const Home = () => {
       <Typography fontSize={36} color="white" fontFamily="impact">
         Semi-Pro Adriatic Rift Championship
       </Typography>
-      <Box
-        position="fixed"
-        display="flex"
-        width="100%"
-        bottom={0}
-        marginBottom={2}
-      >
-        <Stack width="100%" direction="row" spacing={2} justifyContent="center">
-          <img
-            src={instagram}
-            loading="lazy"
-            height={90}
-            width={90}
-            onClick={handleInstagramClick}
-          />
-          <img
-            style={{ marginTop: -7 }}
-            src={tiktok}
-            loading="lazy"
-            height={105}
-            width={105}
-            onClick={handleTiktokClick}
-          />
-          <img
-            style={{ marginTop: -12 }}
-            src={youtube}
-            loading="lazy"
-            height={110}
-            width={90}
-            onClick={handleYoutubeClick}
-          />
-          <img
-            style={{ marginTop: 5 }}
-            src={email}
-            loading="lazy"
-            height={80}
-            width={100}
-            onClick={handleEmailClick}
-          />
-        </Stack>
-      </Box>
     </Box>
   );
 };
