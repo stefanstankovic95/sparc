b9957279f489794d437339edff07197308f256a1
Stefan Stankovic <stefantesla995@gmail.com>
2024-02-22 21:37:48 +0100

    - Fix report

--- sparc-ui/src/containers/LeagueStatistic.tsx
+++ sparc-ui/src/containers/LeagueStatistic.tsx
@@ -1,54 +1,38 @@
-import { PowerBIEmbed } from "powerbi-client-react";
-import { models } from "powerbi-client";
+import { useState } from "react";
 
 export const LeagueStatistic = () => {
+  const [loaded, setLoaded] = useState(false);
+
   return (
     <div
       style={{
         display: "flex",
         justifyContent: "center",
-        marginTop: 100,
+        marginTop: 200,
       }}
     >
-      <PowerBIEmbed
-        embedConfig={{
-          type: "report",
-          id: "1dd5eee4-651f-4d65-9a4a-9cd1c32ed8a2",
-          embedUrl:
-            "https://app.powerbi.com/view?r=eyJrIjoiOGRmMzc1NWMtMDY3Mi00MzVhLWIxZDMtNDYzMDI2NGRlZjEwIiwidCI6IjMxODY4OTk5LWE5NTUtNGNiNi04N2E3LTJmZTYwMDk3Y2IwNyIsImMiOjl9",
-          settings: {
-            panes: {
-              filters: {
-                expanded: false,
-                visible: false,
-              },
-              visualizations: {
-                expanded: false,
-                visible: false,
-              },
-              syncSlicers: {
-                visible: false,
-              },
-              selection: {
-                visible: false,
-              },
-              pageNavigation: {
-                visible: false,
-              },
-              bookmarks: {
-                visible: false,
-              },
-              fields: {
-                expanded: false,
-                visible: false,
-              },
-            },
-            layoutType: models.LayoutType.MobilePortrait,
-            background: models.BackgroundType.Transparent,
-          },
+      <div
+        id="powerbiContainer"
+        style={{
+          display: "flex",
+          visibility: loaded ? "visible" : "hidden",
+          overflow: "hidden",
+          width: 1280,
+          height: 722,
         }}
-        cssClassName="bi-report"
-      />
+      >
+        <iframe
+          title="SparcReport"
+          height={780}
+          width={1280}
+          src="https://app.powerbi.com/view?r=eyJrIjoiZjQyYjRkNDMtZDU3Ny00NWE4LWI2YjUtMGRmMWE0NTEzNDU0IiwidCI6IjMxODY4OTk5LWE5NTUtNGNiNi04N2E3LTJmZTYwMDk3Y2IwNyIsImMiOjl9"
+          allowFullScreen={true}
+          onLoad={() => {
+            console.log("stefan loaded");
+            setLoaded(true);
+          }}
+        ></iframe>
+      </div>
     </div>
   );
 };
